b1e2de1c7db92b486ea2c9da4466c2d9bffe1403
Madina192 <madinaakpaeva000@gmail.com>
2023-05-28 12:24:25 +0600

    created fun to print myName

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,10 @@
 fun main() {
     println(getResult(10, 7, '*'))
     println(getResult(120, 6, '/'))
+    println(getResult(40, 25, '+'))
+    println(getResult(95, 15, '-'))
+    println("PROBLEM #2 ---------")
+    println(getName())
 }
 //1
 fun getResult(a : Int, b : Int, operator : Char) : Int {
@@ -15,5 +19,24 @@ fun getResult(a : Int, b : Int, operator : Char) : Int {
 
 //2
 fun getName() : String {
-    return ""
+    val names = arrayOf("Regina", "Joodar", "Akmaral", "Habib", "Jibek", "Aruuzat")
+    val myName = "madina"
+    var isWordFound : Boolean
+    var emptyString = ""
+    for (i in myName.indices) {
+        for (word in names) {
+            isWordFound = false
+            for (k in word.indices) {
+                if (myName[i] == word[k]) {
+                    emptyString += word[k]
+                    isWordFound = true
+                    break
+                }
+            }
+            if (isWordFound) {
+                break
+            }
+        }
+    }
+    return emptyString
 }
